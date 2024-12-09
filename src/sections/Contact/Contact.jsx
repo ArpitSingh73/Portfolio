6ff5550924ccc719b81da36d7639b2822a9aff89
ArpitSingh73 <arpitsingh73073@gmail.com>
2024-12-09 23:46:13 +0530

    email error handled.

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,10 @@
-import styles from './ContactStyles.module.css';
+import styles from "./ContactStyles.module.css";
 
 function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="https://formspree.io/f/xldekgyj">
+      <form action="https://formspree.io/f/xldekgyj" method="POST">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
