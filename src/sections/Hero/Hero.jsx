fb351e8862eaf1b03dd1ee5e466ad3f182d1f664
ArpitSingh73 <arpitsingh73073@gmail.com>
2024-12-09 23:10:07 +0530

    new links added.

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -4,10 +4,14 @@ import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
 import twitterDark from '../../assets/twitter-dark.svg';
+import gfgLight from '../../assets/gfg-light.svg';
+import gfgDark from '../../assets/gfg-dark.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
+import hashnodeDark from "../../assets/hashnode-dark.svg";
+import hashnodeLight from "../../assets/hashnode-light.svg";
 import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
@@ -15,9 +19,10 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const gfgIcon = theme === 'light' ? gfgLight : gfgDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const hashnodeIcon = theme === "light" ? hashnodeLight : hashnodeDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -42,14 +47,17 @@ function Hero() {
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
+          <a href="https://geeksforgeeks.org/" target="_blank">
+            <img src={gfgIcon} alt="Twitter icon" />
           </a>
           <a href="https://github.com/" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
           <a href="https://linkedin.com/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+          <a href="https://hashnode.com/" target="_blank">
+            <img src={hashnodeIcon} alt="Linkedin icon" />
           </a>
         </span>
         <p className={styles.description}>
